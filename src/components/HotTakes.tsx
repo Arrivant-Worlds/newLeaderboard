71cbb65f7145ad3a57c60508dda6ae61722078d1
Peter <thaiphuongn1@gmail.com>
2021-10-07 14:49:07 +0700

    fix styling

--- src/components/HotTakes.tsx
+++ src/components/HotTakes.tsx
@@ -1,41 +1,54 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions,Container } from '@mui/material';
-import {makeStyles} from '@mui/styles'
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions, Container } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-
-
-
-export default function HotTakes(props:any) {
+export default function HotTakes(props: any) {
   const classes = useStyles();
   return (
-    <Container className = {classes.root}>
-        <Typography className = {classes.title1} align = "center" variant = "h2" color = "CaptionText">Project Eluune</Typography>
-        <Typography  className = {classes.title2} variant = "h4" color = "CaptionText">Decoders Leaderboard</Typography>
-        <Button   variant ="contained" color = "secondary">CLAIM</Button>
+    <Container className={classes.root}>
+      <Typography
+        className={classes.title1}
+        align="center"
+        variant="h2"
+        color="White"
+      >
+        Project Eluune
+      </Typography>
+      <Typography className={classes.title2} variant="h4" color="white">
+        Decoders Leaderboard
+      </Typography>
+      <Button
+        style={{
+          background: "pink",
+          borderRadius: "25px",
+          height: "84px",
+          width: "318px",
+          border: "1px none",
+          fontSize: "293%",
+          color: "white",
+        }}
+      >
+        CLAIM
+      </Button>
     </Container>
   );
 }
 
 const useStyles = makeStyles({
   root: {
-    paddingTop:'150px',
-    backgroundColor: 'none',
-    color:'ButtonShadow',
-    height:'100vh',
-    
-    
+    paddingTop: "150px",
+    backgroundColor: "none",
+    color: "ButtonShadow",
+    height: "100vh",
   },
-  title1:{
-    padding:'10px'
+  title1: {
+    padding: "10px",
   },
-  title2:{
-    padding:'10px'
+  title2: {
+    padding: "10px",
   },
- 
 });
-
-
--- src/components/LeaderBoardCard.tsx
+++ src/components/LeaderBoardCard.tsx
@@ -1,50 +1,54 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions, Link } from '@mui/material';
-import {makeStyles} from '@mui/styles'
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions, Link } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 
-
-
-
-export default function LeaderCard(props:any) {
-  
-
+export default function LeaderCard(props: any) {
   const classes = useStyles();
   return (
-    
-    <Card style = {{backgroundColor:"black"}}sx={{ maxWidth: 345 }} className = {classes.root}>
-      <CardActionArea href = "https://twitter.com/" className = {classes.root}>
+    <Card
+      style={{ backgroundColor: "black" }}
+      sx={{ maxWidth: 345 }}
+      className={classes.root}
+    >
+      <CardActionArea href="https://twitter.com/" className={classes.root}>
         <CardMedia
           component="img"
           height="220"
           image="/images/example.png"
-          className = {classes.root}
+          className={classes.root}
           alt=""
-          
         />
         <CardContent>
           <Typography>
-            <Link href = "#">{props.handle}</Link>
+            <Link href="#">{props.handle}</Link>
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions sx = {{mx:15}}>
-        <Button size="large" color="primary" variant ="outlined" href = "https://twitter.com/compose/tweet?text=@ProjectEluune Claiming my Hot Take.">
+      <CardActions sx={{ mx: 15 }}>
+        <Button
+          size="large"
+          style={{
+            background: "pink",
+            borderRadius: "12px",
+            border: "1px none",
+            color: "white",
+          }}
+          href="https://twitter.com/compose/tweet?text=@ProjectEluune Claiming my Hot Take."
+        >
           Claim
         </Button>
       </CardActions>
     </Card>
-    
   );
 }
 
 const useStyles = makeStyles({
   root: {
-    backgroundColor: 'blue',
-    color:'ButtonShadow'
-    
+    backgroundColor: "blue",
+    color: "ButtonShadow",
   },
 });
